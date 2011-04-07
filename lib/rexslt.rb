b87383eb7edc25108be8be1d50eb61d4044d178b
Your Name <james@r0bertson.co.uk>
2011-04-07 12:21:54 +0100

    implementedxsl:ifandxsl:choose

--- lib/rexslt.rb
+++ lib/rexslt.rb
@@ -8,16 +8,19 @@ class Rexslt
     doc_xml = Document.new(xml)
     doc_xsl = Document.new(xsl)
 
-    @xsl_methods = [:apply_templates, :value_of]
+    @xsl_methods = [:apply_templates, :value_of, :element, :if, :choose, :when]
 
     # fetch the templates
     @templates = XPath.match(doc_xsl.root, 'xsl:template').inject({}) do |r,x|
       r.merge(x.attribute('match').value => x)
     end
 
+    xpath = String.new @templates.to_a[0][0] 
+    o = XPath.first(doc_xml, xpath)
+
     # using the 1st template
-    @a = XPath.match(doc_xml, @templates.to_a[0][0]).map do |x|
-      read_template @templates.to_a[0][-1], x
+    @a = XPath.match(doc_xml, xpath).map do |x|
+      read_node @templates.to_a[0][-1], x
     end
 
   end
@@ -30,29 +33,88 @@ class Rexslt
 
   private
 
-  def read_template(template_node, doc_node)
+  def read_node(template_node, doc_node)
 
-    XPath.match(template_node, '*').map do |x|      
+    procs = {"REXML::Element" => :read_raw_element, "REXML::Text" => :read_raw_text}
+    template_node.map do |x|
+      #puts 'x : ' + x.inspect
+      method(procs[x.class.to_s]).call(doc_node, x)
+    end
+    
+  end
 
-      method_name = x.name.gsub(/-/,'_').to_sym
+  def read_raw_text(doc_node, x)
+    r = x.to_s.strip.length > 0 ? x.to_s : ''
+    #puts 'xx : ' + r.inspect
+    r
+  end
+  
+  def read_raw_element(doc_node, x)
+    method_name = x.name.gsub(/-/,'_').to_sym
+    if @xsl_methods.include? method_name then
 
-      if @xsl_methods.include? method_name then
-        field = x.attribute('select').value.to_s if x.attribute('select')
-        method(method_name).call(doc_node, field)
-      else
+      method(method_name).call(doc_node, x)
+    else
+
+      if x.has_elements? then           
+        element = "<%s>%%s</%s>" % ([x.name.to_s] * 2)
+        k = element % read_node(x, doc_node).flatten.join
+        #puts 'k : ' + k.inspect
+        k
+      else        
         x.to_s
       end
-    end
+    end    
   end
 
-  def apply_templates(doc_node, field)
+  def apply_templates(doc_node, x)
+    field = x.attribute('select').value.to_s
     XPath.match(doc_node, field).map do |x|
-      read_template @templates[field], x
+      read_node @templates[field], x
     end
   end
 
-  def value_of(doc_node, field)
+  def value_of(doc_node, x)
+    field = x.attribute('select').value.to_s
     doc_node.text(field)
   end
 
+  def element(doc_node, x)
+    name = x.attribute('name').value.to_s
+    element = "<%s>%%s</%s>" % ([name] * 2)
+    r = element % (read_node(x, doc_node).flatten.join)
+    #puts 'r : ' + r.inspect
+    r
+  end
+  
+  def if(doc_node, x)
+    condition = x.attribute('test').value.to_s
+    node = XPath.first(doc_node, condition)
+    read_node(x, doc_node).flatten.join if node and node == true
+  end
+
+  def choose(doc_node, x)
+
+    #get the when clause    
+    nodes = XPath.match(x, "xsl:when")
+
+    r = nodes.map do |xsl_node|
+
+      condition = xsl_node.attribute('test').value.to_s
+      node = XPath.first(doc_node, condition)
+
+      if node and node == true then
+        read_node(xsl_node, doc_node).flatten.join       
+      else
+        nil
+      end
+    end
+
+    if r.compact.length <= 0 then
+      xsl_node = XPath.first(x, 'xsl:otherwise')
+      r = read_node(xsl_node, doc_node).flatten.join if xsl_node
+    end
+    
+    r
+  end
 end
